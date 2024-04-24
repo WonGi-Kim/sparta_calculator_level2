10ba7377d4940445821020215246f7726d512647
Robbie <robbie331@>
2024-04-24 20:25:36 +0900

    level2 요구사항 2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,68 @@
 package calculator;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Queue;
+
 public class App {
-
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String finish = new String();
+        String stopQueue = new String();
+        String printQueue = new String(); //요구사항 8 inquiry를 받을 문자열
 
+        int a; // 첫 번째 숫자
+        int b; // 두 번째 숫자
+        String operand; // 연산자
+        int result = 0; // switch 문 밖에서 선언하고 초기화
+
+        //Queue<Integer> resultQueue = new LinkedList<>(); 요구사항에 맞춰 Calculator로 이동
+        Calculator calculator =new Calculator();
+
+        while (!finish.equals("exit")) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            a = in.nextInt();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            operand = in.next();
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            b = in.nextInt();
+
+            try {
+                calculator.calculate(operand,a,b);
+            } catch (DivisionByZeroException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidOperatorException e) {
+                throw new RuntimeException(e);
+            }
+
+            if(calculator.resultQueue.size() >= 2) { // remove는 큐가 비어있는 경우 예외가 발생하므로 예외 처리를 먼저 해줘야 함
+                System.out.print("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력시 삭제)");
+                stopQueue = in.next();
+                if (stopQueue.equals("remove")) {
+                    calculator.resultQueue.remove();
+                }
+            }
+
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력시 조회)");
+            printQueue = in.next();
+            if (printQueue.equals("inquiry")) {
+                for(int value : calculator.resultQueue) { // 요구사항 8 향상된 for문을 사용하여 출력
+                    System.out.print(value + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.print("더 계산하시겠습니까? (계속 계산하시려면 아무키나 입력해주세요. exit 입력 시 종료됩니다.)");
+            finish = in.next();
+            if(finish.equals("exit")) {
+                break;
+            } else {
+                continue;
+            }
+        }
     }
 }
+
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,43 @@
 package calculator;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Calculator {
+    /* 연산 결과를 저장하는 컬랙션 타입 필드 선언 및 생성 */
+    int result;
+    Queue<Integer> resultQueue= new LinkedList<>(); // 연산결과를 저장하는 컬렉션 타입
+
+    // 결과값을 반환하는 메서드
+    public int calculate(String operator, int firstNumber, int secondNumber) throws
+            DivisionByZeroException, InvalidOperatorException{
+        /** 요구사항 1
+         * 양의 정수 2개와 연산 기호를 매개변수로 받아 사칙 연산 기능을 수행한 후 결과 값을 반환
+         * 나눗셈에서 분모에 0이 들어오거나 연산자 기호가 잘못 들어온 경우 Exception 클래스를 생성하여 throw
+         */
+        result = 0;
+
+        if (operator.equals("+")) {
+            result = firstNumber + secondNumber;
+            System.out.println("결과는 : " + result);
+        } else if (operator.equals("-")) {
+            result = firstNumber - secondNumber;
+            System.out.println("결과는 : " + result);
+        } else if (operator.equals("*")) {
+            result = firstNumber * secondNumber;
+            System.out.println("결과는 : " + result);
+        } else if (operator.equals("/")) {
+            if (secondNumber == 0) {
+                // Exception
+                throw new DivisionByZeroException("분모에 0이 들어올 수 없습니다.");
+            }
+            result = firstNumber / secondNumber;
+            System.out.println("결과는 : " + result);
+        } else {
+            throw new InvalidOperatorException("잘못된 연산자 입니다.");
+        }
+        resultQueue.offer(result);
+        return result;
+    }
 
 }
